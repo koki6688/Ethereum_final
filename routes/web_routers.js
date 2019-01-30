887bce314153453aae46b1485872835a927712b3
joehsiao <joehsiao@intumit.com>
2019-01-30 12:08:10 +0800

    login

--- routes/web_routers.js
+++ routes/web_routers.js
@@ -6,6 +6,11 @@ router.get('/',async function(req, res, next) {
     res.render('index', { title: 'Express' });
 });
 
+router.get('/login',async function(req, res, next) {
+    res.render('login', { title: 'login' });
+});
+
+
 router.get('/users',async function (req, res, next) {
     res.render('index', { title: 'test7' });
 });
